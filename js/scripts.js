d14b46bfef519cb6c31e50cb8c98e8830a637967
Ted Leary <markeleary@gmail.com>
2018-03-30 11:38:59 -0700

    apply .string to checkNumber in order to determine if number in string contains 1 or zero, commence serious debugging

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,58 @@
-var displayArray = [];
-
+var counterArray = [];
+var checkNumber = [];
+var fup = [];
+console.log(fup);
 var counter = function(input) {
   for (var checkNumber = 0; checkNumber <= input; checkNumber++) {
-    if  (checkNumber !== 0 && checkNumber % 3 === 0) {
-      displayArray.push("I'm sorry Dave, I'm afraid I can't do that");
-    } else if (checkNumber === 1) {
-      displayArray.push("Beep!");
-    } else if (checkNumber === 0) {
-      displayArray.push("Boop");
-    } else {
-      displayArray.push(checkNumber);
-    }
-
-    }
-
+    counterArray.push(checkNumber);
+    console.log(checkNumber);
   }
+}
+
+var beepBooper = function(numberList) {
+  for (var i = 0; i < numberList.length; i++) {
+    var checkNumber2 = numberList[i];
+      console.log(checkNumber2);
+    if (checkNumber2 !== 0 && checkNumber2 % 3 === 0) {
+      checkNumber.push("I'm sorry Dave, I'm afraid I can't do that");
+    } else if (checkNumber2 === 1) {
+      checkNumber.push("Beep!");
+    } else if (checkNumber2 === 0) {
+      checkNumber.push("Boop!");
+    } else {
+      checkNumber.push(checkNumber2);
+    }
+  }
+}
+
+var furtherBeepBooper = function(stringArray) {
+  for (var i = 0; i < stringArray.length; i++) {
+    var oneZero = stringArray[i];
+    if (oneZero.charAr(0) === "1" ) {
+      fup.push("Beep!");
+    } else {
+
+    }
+  }
+}
+
+  $(document).ready(function() {
+    $("#form1").submit(function(event) {
+      var input = parseInt($("#input1").val());
+
+      counter(input);
+      beepBooper(counterArray);
+
+debugger;
+      var stringCheckNumber = checkNumber.toString();
+      console.log(stringCheckNumber);
+      var stringArray = stringCheckNumber.split(",");
+      furtherBeepBooper(stringArray);
 
 
-// var beepbooper = function(number) {
-//   for (var i = 0; i < number.length; i++) {
-//     var checkNumber = parseInt(number[i]);
-//     console.log(checkNumber);
+      $("#output").append(checkNumber);
 
+      event.preventDefault();
+    });
 
-  //   if (checkNumber !== 0 && checkNumber % 3 === 0) {
-  //     displayArray.push("I'm sorry Dave, I'm afraid I can't do that");
-  //   } else if (checkNumber === 1) {
-  //     displayArray.push("Beep!");
-  //   } else if (checkNumber === 0) {
-  //     displayArray.push("Boop");
-  //   } else {
-  //     displayArray.push(checkNumber);
-  //   }
-  //
-  //   }
-  //
-  // }
-
-
-
-
-
-
-$(document).ready(function() {
-  $("#form1").submit(function(event) {
-    var input = parseInt($("#input1").val());
-
-    counter(input);
-    // beepbooper();
-    console.log(input);
-    $("#output").append(displayArray);
-
-  event.preventDefault();
-});
-
-});
+  });
