1759d2fdb0e8ff12ff5f52af967e60f094258fde
Ted Leary <markeleary@gmail.com>
2018-03-30 09:04:12 -0700

    make conditional statement regarding numbers divisible by 3 print message to output

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,22 @@ var displayArray = [];
 
 var beepbooper = function(number) {
   for (var i = 0; i < number.length; i++) {
-    var checkNumber = parseInt(number[i];)
-    if (checkNumber % 3 === 0); {
-      $("#output").append
+    var checkNumber = parseInt(number[i]);
+    console.log(checkNumber);
+    debugger;
+
+    if (checkNumber !== 0 && checkNumber % 3 === 0) {
+      $("#output").append("<li>" + "I'm sorry Dave, I'm afraid I can't do that.");
+    } else {
+
     }
 
-    console.log(checkNumber);
+  }
+
+
 
   }
-}
+
 
 $(document).ready(function() {
   $("#form1").submit(function(event) {
