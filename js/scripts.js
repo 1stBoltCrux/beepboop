703b92ff951c23d9eda5ec1b57d449f6eb3ef1e1
Ted Leary <markeleary@gmail.com>
2018-03-30 09:36:15 -0700

    add counter to count up to user inserted number, apply previous conditional statement to counter, comment out incorrect function

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,8 @@
 var displayArray = [];
 
-
-var beepbooper = function(number) {
-  for (var i = 0; i < number.length; i++) {
-    var checkNumber = parseInt(number[i]);
-    console.log(checkNumber);
-    debugger;
-
-    if (checkNumber !== 0 && checkNumber % 3 === 0) {
+var counter = function(input) {
+  for (var checkNumber = 0; checkNumber <= input; checkNumber++) {
+    if  (checkNumber !== 0 && checkNumber % 3 === 0) {
       displayArray.push("I'm sorry Dave, I'm afraid I can't do that");
     } else if (checkNumber === 1) {
       displayArray.push("Beep!");
@@ -22,17 +17,39 @@ var beepbooper = function(number) {
   }
 
 
+// var beepbooper = function(number) {
+//   for (var i = 0; i < number.length; i++) {
+//     var checkNumber = parseInt(number[i]);
+//     console.log(checkNumber);
+
+
+  //   if (checkNumber !== 0 && checkNumber % 3 === 0) {
+  //     displayArray.push("I'm sorry Dave, I'm afraid I can't do that");
+  //   } else if (checkNumber === 1) {
+  //     displayArray.push("Beep!");
+  //   } else if (checkNumber === 0) {
+  //     displayArray.push("Boop");
+  //   } else {
+  //     displayArray.push(checkNumber);
+  //   }
+  //
+  //   }
+  //
+  // }
+
+
 
 
 
 
 $(document).ready(function() {
   $("#form1").submit(function(event) {
-    var input = $("#input1").val();
-    inputArray = input.split("");
-    beepbooper(inputArray);
-    console.log(inputArray);
-    $("#output").append(displayArray)
+    var input = parseInt($("#input1").val());
+
+    counter(input);
+    // beepbooper();
+    console.log(input);
+    $("#output").append(displayArray);
 
   event.preventDefault();
 });
