1ac5baf283b94a875373c5f1b1a5f723105b8fdf
Ted Leary <markeleary@gmail.com>
2018-03-30 15:36:20 -0700

    revert back to relying on simpler conditional statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var counterArray = [];
 var checkNumber = [];
-var furtherArray =[];
+// var furtherArray =[];
 var counter = function(input) {
   for (var checkNumber = 0; checkNumber <= input; checkNumber++) {
     counterArray.push(checkNumber);
@@ -14,26 +14,34 @@ var beepBooper = function(numberList) {
       console.log(checkNumber2);
     if (checkNumber2 !== 0 && checkNumber2 % 3 === 0) {
       checkNumber.push("I'm sorry Dave I'm afraid I can't do that");
+
+    } else if (checkNumber2 === 1 || checkNumber2 % 10 === 1) {
+      checkNumber.push("Beep!");
+
+    } else if (checkNumber2 === 0) {
+      checkNumber.push("Boop!");
     } else {
       checkNumber.push(checkNumber2);
     }
   }
 }
 
-  var furtherBeepBooper = function(final) {
-  for (var i = 0; i < final.length; i++) {
-    debugger;
-    var checkNumber3 = final[i];
-    var wtf = checkNumber3.toString();
-    wtf.charAt(i);
-    console.log(checkNumber3);
-    if (wtf == "1") {
-      alert("hooray!!!!!!!");
-    } else {
-      alert("booooo!");
-    }
-  }
-}
+//   var furtherBeepBooper = function(final) {
+//   for (var i = 0; i < final.length; i++) {
+//     debugger;
+//     var checkNumber3 = final[i];
+//     var wtf = checkNumber3.toString();
+//     var position = (final.length - 1)
+//
+//     var wtf2 = wtf.charAt(0);
+//     console.log(checkNumber3);
+//     if (wtf2 == "1") {
+//
+//     } else {
+//       alert("booooo!");
+//     }
+//   }
+// }
 
   $(document).ready(function() {
     $("#form1").submit(function(event) {
@@ -41,10 +49,10 @@ var beepBooper = function(numberList) {
 
       counter(input);
       beepBooper(counterArray);
-      var stringNumber = checkNumber.toString();
-      var final = stringNumber.split(",")
-      console.log(final);
-      furtherBeepBooper(checkNumber)
+      // var stringNumber = checkNumber.toString();
+      // var final = stringNumber.split(",")
+      // console.log(final);
+      // furtherBeepBooper(checkNumber)
 
 
       $("#output").append(checkNumber);
