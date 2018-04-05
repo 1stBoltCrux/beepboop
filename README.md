7dc5327d6ce7726936a50ed15a5623cb521a75ee
Ted Leary <markeleary@gmail.com>
2018-04-04 22:07:57 -0700

    restructure BDD Breakdown, change variable names in JS to be more clear, add notes within JS to further clarify intent

--- README.md
+++ README.md
@@ -7,24 +7,34 @@
 
 ## _Behavior Driven Development Breakdown_
 
-* _1. Program replaces 0 with "Beep"
-input: 0
+* _1. Program takes in a value and outputs numbers from 0 up to the inputted value.
+input: 8
+output: [1, 2, 3, 4, 5, 6, 7, 8]_
+
+* _2. Program replaces 0 with "Beep"
+input: [0]
 output "Beep"_
-* _2. Program replaces 1 with "Boop"
-input: 1
-output: "Boop"_
-* _3. Program identifies a string or number containing 0 and replaces with "Beep"
-input: 123450
-output: "Beep"_
-* _4. Numbers divisible by three will be replaced with "I'm sorry Dave, I'm afraid I can't do that."
-input: 9
-output: "I'm sorry Dave, I'm afraid I can't do that."_
-* _5. Program should determine which action to take on a number with both 1's, 0's and numbers divisible by 3 in an order of importance.
-input: 30
-output "I'm sorry Dave, I'm afraid I can't do that."_
-* _6. Program will display user input with 1's replaced with "Boop" and 0's replaced with "Beep" and all numbers divisible by 3 replaced with "I'm sorry Dave, I'm afraid I can't do that."
-input: 10
-output: "Beep"_
+
+* _3. Program replaces 1 with "Boop"
+input: [0, 1]
+output: "Beep, Boop"_
+
+* _4. Program identifies a string or number containing 0 and replaces with "Beep"
+input: [ .... 10]
+output: "...Beep"_
+
+* _5. Program identifies a string or number containing a 1 and replaces with "Boop"
+input: [....12]
+output: "...Boop"_
+
+* _6. Numbers divisible by three will be replaced with "I'm sorry Dave, I'm afraid I can't do that."
+input: [0, 1, 2, 3]
+output: "Beep, Boop, 2, I'm sorry Dave, I'm afraid I can't do that."_
+
+* _7. Program should determine which action to take on a number with both 1's, 0's and numbers divisible by 3 in an order of importance.
+input: [0...10, ... 13, ....30]
+output "Beep, ...Boop, ...Boop, I'm sorry Dave, I'm afraid I can't do that.""
+
 
 
 ## The purpose of this website is to allow the user to input a number to be translated into beeps and boops.
--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,62 @@
 // -----------------BACK END-----------------
 
 var counterArray = [];
+var parentArray = [];
 var checkAgain =[];
-var arrayX = [];
 var finalArray = [];
+
+// "counter" function takes in value from input and creates an array from 0 to that input value //
 
 var counter = function(input) {
   for (var checkNumber = 0; checkNumber <= input; checkNumber++) {
     counterArray.push(checkNumber);
   }
 }
+// "arrayPlacer" function pushes every element within counterArray into "parentArray" then pushes "parentArray" into the "checkAgain" array, then "parentArray" is reset to empty. This creates an array of arrays which will then be run through a loop within the "arrayChecker" function //
+
 var arrayPlacer = function(array) {
   for (var i = 0; i < array.length; i++) {
     var pushArray = array[i];
-    arrayX.push(pushArray);
-    checkAgain.push(arrayX);
-    arrayX = [];
+    parentArray.push(pushArray);
+    checkAgain.push(parentArray);
+    parentArray = [];
   }
 }
 
+// "arrayChecker" cycles through each array within "checkAgain", changes some of those arrays to strings and keeps some of them as numeric values, then passes them through a conditional statement //
+
 var arrayChecker = function(checkAgain) {
   for (var i = 0; i < checkAgain.length; i++) {
-    var arrayArray = checkAgain[i];
-    var arrayArray2 = arrayArray.toString();
-    if (arrayArray > 2 && arrayArray % 3 === 0) {
+    var numberSelect = checkAgain[i];
+    var stringSelect = numberSelect.toString();
+    if (numberSelect > 2 && numberSelect % 3 === 0) {
       finalArray.push(" I'm sorry " + "<span class='name'></span>" + "," + " I'm afraid I can't do that ");
-    } else if (arrayArray2.indexOf("1") !== -1) {
-    finalArray.push(" Boop! ");
-    } else if (arrayArray2.indexOf("0") !== -1) {
+    } else if (stringSelect.indexOf("1") !== -1) {
+      finalArray.push(" Boop! ");
+    } else if (stringSelect.indexOf("0") !== -1) {
       finalArray.push(" Beep! ");
     } else {
-      finalArray.push(" " + arrayArray + " ");
+      finalArray.push(" " + numberSelect + " ");
     }
   }
 }
 // --------------FRONT END-----------------------
 
-  $(document).ready(function() {
-    $(".dave").hide();
-    $("#form1").submit(function(event) {
-        event.preventDefault();
-      var input = parseInt($("#input1").val());
-      var input2 = $("#input2").val();
-      counter(input);
-      arrayPlacer(counterArray);
-      arrayChecker(checkAgain);
-      $(".dave").show();
-      $("#hal").hide();
-      $("#output").append(finalArray);
-      $(".name").text(input2);
+$(document).ready(function() {
+  $(".dave").hide();
+  $("#form1").submit(function(event) {
+    event.preventDefault();
+    var input = parseInt($("#input1").val());
+    var input2 = $("#input2").val();
+    counter(input);
+    arrayPlacer(counterArray);
+    arrayChecker(checkAgain);
+    $(".dave").show();
+    $("#hal").hide();
+    $("#output").append(finalArray);
+    $(".name").text(input2);
 
-
-    });
 
   });
+
+});
